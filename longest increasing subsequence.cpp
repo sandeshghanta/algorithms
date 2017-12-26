209e2f5e8e5ea888bfb0831f5fea7c8658c117e2
Sandesh Ghanta <sghanta05@gmail.com>
2017-12-26 00:05:34 -0500

    Longest Increasing Subsequence
    Finds the longest increasing subsequence in an array in o(n^2) complexity.

---
+++ longest increasing subsequence.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int n,el,i,j,max;
+	cout << "Enter size of array " << endl;
+	cin >> n;
+	int data[n];
+	cout << "Enter the elements " << endl;
+	for (i=0;i<n;i++){
+		cin >> el;
+		data[i] = el;
+	}
+	int ans[n];
+	ans[0] = 1;
+	for (i=1;i<n;i++){
+		max = -1;
+		for (j=0;j<i;j++){
+			if (data[i] > data[j] && ans[j] + 1> max){
+				max = ans[j] + 1;
+			}
+		}
+		ans[j] = max;
+	}
+	max = -1;
+	for (i=0;i<n;i++){
+		if (max < ans[i]){
+			max = ans[i];
+		}
+	}
+	cout << "Longest increasing subsequence is " << max << endl;
+	return 0;
+}
